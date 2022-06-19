1061f6c715ee021b5d87586b5990353f69386540
Dylan Jordan <kyle.dylan.jordan@gmail.com>
2022-06-19 08:36:01 -0400

    Initial Commit

---
+++ src/Components/shape.js
@@ -0,0 +1,76 @@
+
+
+const SHAPES = [
+    //square
+    {
+        shape: [
+            {x:0, y:0},
+            {x:0, y:1},
+            {x:1, y:0},
+            {x:1, y:1}
+        ],
+        width: 2,
+        height: 2,
+        rotate: false
+    },
+    //long
+    {
+        shape: [
+            {x:0, y:0},
+            {x:0, y:1},
+            {x:0, y:2},
+            {x:0, y:3}
+        ],
+        width: 1,
+        height: 4
+    },
+    //
+    {
+        shape: [
+            {x:0, y:0},
+            {x:0, y:1},
+            {x:0, y:2},
+            {x:1, y:2}
+        ],
+        width: 1,
+        height: 3
+    },
+    // L
+    {
+        shape: [
+            {x:1, y:0},
+            {x:0, y:1},
+            {x:1, y:2},
+            {x:1, y:1}
+        ],
+        width: 2,
+        height: 3
+    },
+    // T shape
+    {
+        shape: [
+            {x:1, y:0},
+            {x:0, y:1},
+            {x:1, y:2},
+            {x:1, y:1}
+        ],
+        width: 2,
+        height: 3
+    },
+    //Reverse L
+    {
+        shape: [
+            {x:1, y:0},
+            {x:0, y:0},
+            {x:0, y:2},
+            {x:0, y:1}
+        ],
+        width: 2,
+        height: 3
+    }
+    // L
+];
+
+export function randomShape() {
+    return SHAPES[Math.floor(Math.random() * SHAPES.length)];
+}

f512c37d9159828d2d268525e144fa0002649e6e
Dylan Jordan <kyle.dylan.jordan@gmail.com>
2022-06-20 22:43:22 -0400

    enabled routing which will make future work easier

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,7 @@
 import Board from "./board.js";
+import '../App.css';
+import ReactDom from "react-dom"
+import { Route, Switch, NavLink } from 'react-router-dom';
 
 function App() {
     return (
@@ -7,9 +10,22 @@ function App() {
                 <h1>tetr.js</h1>
                 <p>by <a href="https://github.com/montgomerykate">K. Montgomery</a> and <a href="https://github.com/flubber2077">D. Jordan</a></p>
             </header>
-            <div className="tetris-parent" >
-                <Board />
-            </div>
+
+            <Switch>
+                <Route path="/h">
+                    <div className="tetris-parent" >
+                        <Board />
+                    </div>
+                </Route>
+
+                <Route path='/'>
+                    <div className='start-button'>
+                        <NavLink to ='/h'>
+                            start
+                        </NavLink>
+                    </div>
+                </Route>
+            </Switch>
         </div>
     );
 }
